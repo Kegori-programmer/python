4d9a074a396eefccb0074ec37bc1d86dfb7e3161
kegori <kosenko.egor.ivanovich@gmail.com>
2021-08-17 15:49:20 +0300

    added questions

--- exercise-project-mysql.py
+++ exercise-project-mysql.py
@@ -1,15 +1,1 @@
-import mysql.connector
-
-con = mysql.connector.connect(
-    user="ardit700_student",
-    password="ardit700_student",
-    host="108.167.140.122",
-    database="ardit700_pm1database"
-)
-
-cursor = con.cursor()
-
-query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'lining' ")
-results = cursor.fetchall()
-
-print(results[0])
+import json
