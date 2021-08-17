5cea30fafaf1808841d95582ed0439896ead0619
ivan <foxartorg@gmail.com>
2021-08-17 15:54:53 +0300

    fiix

--- basics.py
+++ basics.py
@@ -3,9 +3,10 @@ import json
 data = json.load(open("files/question.json", encoding='utf8'))
 # print(data)
 
-for questionIndex, question in enumerate(data):
-    print(questionIndex, question)
+for qIndex, question in enumerate(data):
+    print(qIndex, question)
     for key in question:
-        print(questionIndex, key)
+        print(qIndex, key)
         for answerIndex, answer in enumerate(question[key]):
-            print(' ', answerIndex, answer)
+            print('test')
+            # print(' ', answerIndex, answer)
